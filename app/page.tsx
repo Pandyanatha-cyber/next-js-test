1b802ca93c7e237d6249ebb7269dc454a36fb65d
Pandyanatha <argantapandyanatha@gmail.com>
2026-07-14 13:44:44 +0700

    tried something

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center w-full bg-[url('/images/shocked-surprised.gif')] bg-contain bg-right bg-no-repeat">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,9 +12,16 @@ export default function Home() {
           height={20}
           priority
         />
+        <span className="box-decoration-slice bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white">
+          Hello<br/>World
+        </span>
+
+        <span className="box-decoration-clone bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white ">
+          Hello<br/>World
+        </span>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, edit the page.tsx file. ඞ
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -58,6 +65,9 @@ export default function Home() {
           >
             Documentation
           </a>
+          <p>
+            amogus
+          </p>
         </div>
       </main>
     </div>
